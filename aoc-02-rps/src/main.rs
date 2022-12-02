81d1782575004b181188f51dd2a67a6aab3c2ea6
Thomas Cover <tom.m.cover@gmail.com>
2022-12-02 09:06:56 -0500

    Refactor to HashMaps of HashMaps

--- aoc-02-rps/src/main.rs
+++ aoc-02-rps/src/main.rs
@@ -20,6 +20,20 @@ fn main() {
 }
 
 fn evaluate_round(elf: &str, player: &str) -> i32 {
+    let points: HashMap<&str, HashMap<&str, i32>> = HashMap::from([
+        (
+            "Z",
+            HashMap::from([("A", 2 + 6), ("B", 3 + 6), ("C", 1 + 6)]),
+        ),
+        (
+            "Y",
+            HashMap::from([("A", 1 + 3), ("B", 2 + 3), ("C", 3 + 3)]),
+        ),
+        ("X", HashMap::from([("A", 3), ("B", 1), ("C", 2)])),
+    ]);
+
+    return points[player][elf];
+    /*
     let wins_points = HashMap::from([("A", 2), ("B", 3), ("C", 1)]);
 
     let draws_points = HashMap::from([("A", 1), ("B", 2), ("C", 3)]);
@@ -32,5 +46,5 @@ fn evaluate_round(elf: &str, player: &str) -> i32 {
         return draws_points[elf] + 3;
     } else {
         return loss_points[elf];
-    }
+    } */
 }
