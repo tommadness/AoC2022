14bf6a1f7b5479bd30c506db07aa80f81dce787f
Thomas Cover <tom.m.cover@gmail.com>
2022-12-02 09:48:52 -0500

    Phil's Tips!

--- aoc-02-rps/src/main.rs
+++ aoc-02-rps/src/main.rs
@@ -2,24 +2,6 @@ use std::collections::HashMap;
 use std::fs;
 
 fn main() {
-    let file_path = format!("{}/input.txt", env!("CARGO_PKG_NAME"));
-    let contents = fs::read_to_string(file_path).expect("Failed to read!");
-
-    let rounds: Vec<Vec<&str>> = contents
-        .trim()
-        .split("\n")
-        .map(|x| x.trim().split(" ").collect::<Vec<&str>>())
-        .collect();
-
-    let mut total_points = 0;
-    for round in rounds {
-        total_points += evaluate_round(round[0], round[1]);
-    }
-
-    println!("{total_points}");
-}
-
-fn evaluate_round(elf: &str, player: &str) -> i32 {
     let points: HashMap<&str, HashMap<&str, i32>> = HashMap::from([
         (
             "Z",
@@ -35,19 +17,14 @@ fn evaluate_round(elf: &str, player: &str) -> i32 {
         ),
     ]);
 
-    return points[player][elf];
-    /*
-    let wins_points = HashMap::from([("A", 2), ("B", 3), ("C", 1)]);
+    let file_path = format!("{}/input.txt", env!("CARGO_PKG_NAME"));
+    let total_points: i32 = fs::read_to_string(file_path)
+        .expect("Failure!")
+        .trim()
+        .split("\n")
+        .flat_map(|x| x.trim().split_once(' '))
+        .map(|(e, p)| points[p][e])
+        .sum();
 
-    let draws_points = HashMap::from([("A", 1), ("B", 2), ("C", 3)]);
-
-    let loss_points = HashMap::from([("A", 3), ("B", 1), ("C", 2)]);
-
-    if player == "Z" {
-        return wins_points[elf] + 6;
-    } else if player == "Y" {
-        return draws_points[elf] + 3;
-    } else {
-        return loss_points[elf];
-    } */
+    println!("{total_points}");
 }
